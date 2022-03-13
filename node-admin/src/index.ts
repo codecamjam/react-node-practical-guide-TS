49543c3e4772bfb8a5fe61513c08647eb338ed74
codecamjam <camerontaghlabi@gmail.com>
2022-03-13 18:22:14 -0400

    l04: express

--- node-admin/src/index.ts
+++ node-admin/src/index.ts
@@ -1,1 +1,15 @@
-console.log('hello');
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(express.json());
+app.use(cors({ origin: ['http://localhost:3000'] }));
+
+app.get('/', (req: Request, res) => {
+  res.send('Hello world');
+});
+
+app.listen(8000, () => {
+  console.log('listening to port 8000');
+});
